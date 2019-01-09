4808b1907c5a50445a84978964f08415827d6f3d
Vinayak-Arora <43749406+Vinayak-Arora@users.noreply.github.com>
2019-01-09 20:16:12 +0530

    Add files via upload

---
+++ Code.R
@@ -0,0 +1,45 @@
+library(dplyr)
+#update directory of the data.csv file -
+dest <- "C://Workspace//data.csv"
+
+df <- read.csv(dest, stringsAsFactors = FALSE)
+#Remove empty rows
+df <- na.omit(df)
+#for connected = true, dataframe is named - df.true
+df.true <- df[df$connected == 'true', ]
+
+#Introduce a new column - p2p_cdn, whose value is - (p2p/p2p+cdn)
+df.true$p2p_cdn <- df.true$p2p/(df.true$cdn+df.true$p2p)
+
+#Loop to create dataframes to check performances of stream_id, browser and isp on the basis of percentage of rows with p2p =0
+#isp.p2p0.df contains percentage values for each isp, and so on for number of rows having p2p = 0
+for(cname in colnames(df.true)[1:3]) {
+  quantile.df <-  data.frame(matrix(nrow = 0, ncol = 4))
+  for(value in as.factor(unique(df.true[ , cname]))) {
+    total <-  df.true[df.true[,cname] == value, ]  %>%
+      nrow()
+    p2p0  <-  df.true[df.true$p2p == 0 & df.true[,cname] == value, ]  %>%
+      nrow()
+
+    percent <- round(((p2p0*100)/total), digits = 2) 
+    quantile.df <- rbind(quantile.df , cbind(value,total, p2p0, percent))
+  }
+  rownames(quantile.df) <- NULL
+  colnames(quantile.df) <- c(cname, 'Total_rows', 'p2p0_rows', 'Percentage')
+  assign(paste(cname,'p2p0','df', sep = '.'), quantile.df)
+}
+
+#Remove rows having value p2p_cdn = 0
+df.true <- df.true[df.true$p2p_cdn > 0 , ]
+
+#Loop to create dataframes to check performances of stream_id, browser and isp on the basis of quantiles
+#isp.quantile.df contains quantile values for each isp, and so on.
+for(cname in colnames(df.true)[1:3]) {
+  quantile.df <-  data.frame(matrix(nrow = 0, ncol = 5))
+    for(value in as.factor(unique(df.true[ , cname]))) {
+      q <- round(quantile(df.true[df.true[ , cname] == value , 'p2p_cdn']), digits = 5)
+      quantile.df <- rbind(quantile.df, cbind(value, q[2], q[3], q[4], sum(df.true[ , cname] == value)))
+    }
+  colnames(quantile.df) <- c(cname, 'Q1', 'Median', 'Q3', 'Number_of_rows')
+  assign(paste(cname,'quantile','df', sep = '.'), quantile.df)
+}
---
+++ Plots.R
@@ -0,0 +1,27 @@
+#Please execute the file Code.R before executing this one.
+
+library(dplyr)
+library(ggplot2)
+
+dest <- "C://Workspace//StreamRoot//data.csv"
+df <- read.csv(dest, stringsAsFactors = FALSE)
+
+df <- na.omit(df)
+#for connected = true, dataframe is named - df.true
+df.true <- df[df$connected == 'true', ]
+#p2p_cdn <- df.true$p2p/df.true$cdn
+df.true$p2p_cdn <- df.true$p2p/(df.true$cdn+df.true$p2p)
+#Remove p2p = 0 values from the dataset for quantile values analysis
+df.true <- df.true[df.true$p2p_cdn > 0 , ]
+
+df.true$X.stream <- as.factor(as.character(df.true$X.stream))
+
+#Plotting boxplots for isp, browser and stream_id to represent p2p_cdn quantile values
+print(ggplot(df.true, aes(browser, p2p_cdn))+stat_boxplot(geom ='errorbar') + geom_boxplot(aes(fill=(browser)),outlier.shape =NA))
+print(ggplot(df.true, aes(isp, p2p_cdn))+stat_boxplot(geom ='errorbar') +  geom_boxplot(aes(fill=(isp)),outlier.shape =NA))
+print(ggplot(df.true, aes(X.stream, p2p_cdn))+stat_boxplot(geom ='errorbar') +  geom_boxplot(aes(fill=(X.stream)),outlier.shape =NA) + guides(fill=FALSE) + labs( x = 'stream_id'))
+
+#Plotting barplots for isp, browser and stream_id to represent percentage of p2p_cdn = 0 values
+print(ggplot(browser.p2p0.df,aes(browser, as.numeric(as.character(Percentage))))+geom_bar(stat = 'identity', aes(fill= browser))+ geom_text(aes(label = Percentage), vjust = -.4) + labs(y = 'p2p0 Percentage'))
+print(ggplot(isp.p2p0.df,aes(isp, as.numeric(as.character(Percentage))))+geom_bar(stat = 'identity', aes(fill= isp))+ geom_text(aes(label = Percentage), vjust = -.4) + labs(y = 'p2p0 Percentage'))
+print(ggplot(X.stream.p2p0.df,aes(X.stream, as.numeric(as.character(Percentage))))+geom_bar(stat = 'identity', aes(fill= X.stream))+ geom_text(aes(label = Percentage), vjust = -.4) + labs(y = 'p2p0 Percentage', x = 'stream_id', colour = 'stream_id') + guides(fill=FALSE))
